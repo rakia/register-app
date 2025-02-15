af7d36836ecf069d151209bc853bcb6364d5444f
Rakia Ben Sassi <rakia.bensassi@webenius.com>
2025-02-15 14:10:27 +0100

    adjust comment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // Redirect empty path to 'sign-up' view
+  // Redirect empty path to 'register' view
   { path: '', loadChildren: () => import('./components/register.routes') },
 ];
